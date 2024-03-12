8f3c225a21c89435c5b955268b08bc727acfbaca
191204taianh <anhnt.22bi13028@usth.edu.vn>
2024-03-13 01:49:21 +0700

    Labwork5

---
+++ pw5/input.py
@@ -0,0 +1,11 @@
+def input_student_num():
+    return int(input("Input the number of students: "))
+
+def input_course_num():
+    return int(input("Input the number of courses: "))
+
+def input_student_info():
+    return input("Enter student name: "), input("Enter student ID: "), input("Enter student date of birth (DD-MM-YYYY): ")
+
+def input_course_info():
+    return input("Enter course name: "), input("Enter course ID: ")
---
+++ pw5/main.py
@@ -0,0 +1,8 @@
+from domains import SchoolSystem
+
+def main():
+    school_system = SchoolSystem()
+    school_system.main()
+
+if __name__ == "__main__":
+    main()
